86d5fb548e4c633a9b99336a617f286d0533cb27
Mohamed Taher <mo.taher717@gmail.com>
2024-08-26 04:02:45 +0300

    views and urls

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#function based view
+from .models import Book
+
+def books_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+
+#class based view
+from django.views.generic.detail import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
