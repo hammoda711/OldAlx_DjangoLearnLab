dc05626d6ae6c2f68f5f1bce188f06a11b0c9661
Mohamed Taher <mo.taher717@gmail.com>
2024-09-08 12:10:18 +0300

    Implement Role-Based Access Control in Django

--- django-models/relationship_app/admin.py
+++ django-models/relationship_app/admin.py
@@ -1,27 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
 
-from .models import Author, Book, Library, Librarian
-
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')
-    list_filter = ('author',)
-    search_fields = ('title', 'author__name')
-
-@admin.register(Library)
-class LibraryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    filter_horizontal = ('books',)  # To add books to library
-    search_fields = ('name',)
-
-@admin.register(Librarian)
-class LibrarianAdmin(admin.ModelAdmin):
-    list_display = ('name', 'library')
-    search_fields = ('name', 'library__name')
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+admin.site.register(CustomUser, CustomUserAdmin)
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,72 +1,108 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-#function based view
-from .models import Book
-#class based view
-from django.views.generic.detail import DetailView
-from .models import Library
-#auth views
-from django.contrib.auth.decorators import user_passes_test
+# relationship_app/views.py
+
 from django.shortcuts import render, redirect
+from .models import Library, Book
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Create your views here.
-
-#function based view
-
-def books_list(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
-
-
-
-#class based view
+def list_books(request):
+    books = Book.objects.all()  # Query to get all books
+    context = {
+        'books': books,
+    }
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_object(self):
+        return Library.objects.get(name = "Cairo")
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add the list of books to the context
-        context['books'] = self.object.books.all()
+        library = self.get_object()
+        books = library.books.all()
+        context['books'] = books
         return context
     
-
-
-#auth views
-
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('login')  # Redirect to a login page 
+            login(request, user)  # Automatically log in the user after registration
+            return redirect(reverse_lazy('login'))  # Redirect to the login page
     else:
         form = UserCreationForm()
     
     return render(request, 'relationship_app/register.html', {'form': form})
 
+# views.py
 
-
-
-def check_admin(user):
+def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-def check_librarian(user):
+def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 
-def check_member(user):
+def is_member(user):
     return user.userprofile.role == 'Member'
 
-@user_passes_test(check_admin)
+@user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(check_librarian)
+@user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
-@user_passes_test(check_member)
+@user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
+
+
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book-list')
+    else:
+        form = BookForm()
+    return render(request, 'relationship_app/add_book.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book-list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/edit_book.html', {'form': form})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book-list')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
+
